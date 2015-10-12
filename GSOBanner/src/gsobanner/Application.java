9158f8362efa3a973257220c432f9e3522ff381f
BartMemelink <b.memelink@student.fontys.nl>
2015-10-12 21:02:07 +0200

    Setup complete

---
+++ GSOBanner/src/gsobanner/Application.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsobanner;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author nickbijmoer
+ * @author Bart Memelink
+ */
+public class Application extends AEXBanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void start(Stage primaryStage)
+    {
+        
+    }
+    
+    public void Stop()
+    {
+        
+    }
+    
+}
